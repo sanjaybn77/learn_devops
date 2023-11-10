6ad2c4e895e5dc0fcda2cf5799d7bdc0c2fce2df
Sanjay bn <62074780+sanjaybn77@users.noreply.github.com>
2023-11-10 10:16:23 +0530

    Update provider.tf

--- terraform/ec2_instance/provider.tf
+++ terraform/ec2_instance/provider.tf
@@ -2,10 +2,5 @@ provider "aws"{
 access_key =""
 secret_key =""
 profile="default"
-<<<<<<< HEAD
 region="ap-northeast-1"
 }
-=======
-region="ap-southeast-2"
-}
->>>>>>> b68f1262549e3e6e67b3056d9963bc86e4656acb
