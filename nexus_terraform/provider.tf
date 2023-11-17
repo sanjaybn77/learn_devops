e7fc36b08d0a3ba6a744d17fd633581ffcb3020d
Sanjay bn <62074780+sanjaybn77@users.noreply.github.com>
2023-11-17 15:19:48 +0530

    Update provider.tf

--- nexus_terraform/provider.tf
+++ nexus_terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws"{
-access_key ="AKIAWXBOVDKFLV23DROS"
-secret_key ="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"
+access_key =""
+secret_key =""
 profile="default"
 region="ap-northeast-1"
 }
